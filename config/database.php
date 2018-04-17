cef6b9cd4d669cd25d1fb0dfbcf4926621ca09f6
kgonzalez452 <kgonzalez@techlaunch.io>
2018-04-16 20:04:52 -0400

    created core and db configs

---
+++ config/database.php
@@ -0,0 +1,20 @@
+<?php
+class Database{
+    //specify your own db credentials
+    private $host = 'localhost';
+    private $db_name = 'php_react_crud';
+    private $username = 'root';
+    private $password = 'Extremejello14';
+    public $connect;
+    
+    public function getConnection() {
+        $this->connect = null;
+
+        try {
+            $this->connect= new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+        } catch(PDOException $exception) {
+            echo 'Connection error:' . $exception->getMessage();
+        }
+        return $this->connect;
+    }
+}
